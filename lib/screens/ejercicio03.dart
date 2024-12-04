f19c10a3cd92b8be7232170f0a4e9332d6abfe21
unknown <jroserog@itsqmet.edu.ec>
2024-12-03 20:19:50 -0500

    ejercicio dolar

--- lib/screens/ejercicio03.dart
+++ lib/screens/ejercicio03.dart
@@ -9,7 +9,45 @@ class Ejercicio03 extends StatelessWidget {
       appBar: AppBar(
         title: Text("Ejercicio 03"),
       ),
-      body: Text("Ejercicio 03")
+      body: Column(
+        children: [
+            Text("Ejercicio 03"),
+            dolar_input(),
+            calcular_btn(context)
+        ],
+      )
     );
   }
+}
+
+TextEditingController _dolar = TextEditingController();
+Widget dolar_input(){
+  return TextField(
+    controller: _dolar,
+  );
+}
+
+Widget calcular_btn(context){
+  return IconButton(onPressed: ()=>calcular(context), 
+  icon: Icon(Icons.calculate_rounded, size: 75, color: Color.fromRGBO(18, 108, 199, 1),)
+  );
+}
+
+void calcular(context){
+  showDialog(context: context, builder: (context){
+    return AlertDialog(
+      title: Text("Respuesta"),
+      content: Text(transformacion(), style: TextStyle(fontSize: 20),),
+    );
+  });
+}
+
+
+String transformacion(){
+  double dolar = double.parse(_dolar.text);
+
+  double euro = dolar * 0.95;
+  double yen = dolar * 127.9;
+
+  return "$dolar USD en euros es $euro \n$dolar en yenes es $yen ";
 }
